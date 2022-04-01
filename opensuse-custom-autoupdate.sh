d49494683a807f22935c10a21a877970bd21281a
powerjungle <fabsidereal@gmail.com>
2022-04-02 01:30:13 +0200

    converted spaces to tabs and fixed file modes

--- opensuse-custom-autoupdate.sh
+++ opensuse-custom-autoupdate.sh
@@ -2,12 +2,12 @@
 
 # Check all needed variables which are not in this script
 check_variable() {
-        if [ -z ${!1} ];
-        then
-                echo "Variable $1 is not set"
-                echo "Should contain $2"
-                exit
-        fi
+	if [ -z ${!1} ];
+	then
+		echo "Variable $1 is not set"
+		echo "Should contain $2"
+		exit
+	fi
 }
 
 check_variable "NOTIFYUSER" "username of the user who will be notified for results"
@@ -18,46 +18,46 @@ USERID=$(id --user $NOTIFYUSER)
 
 # Notification functions
 notify_user() {
-        if [ "$NOGUI" != "yes" ]; 
-        then
-	        sudo -u $NOTIFYUSER \
-                        DISPLAY=:0 \
-                        DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$USERID/bus \
-                        notify-send --urgency=$3 "$1" "$2"
-        fi
+	if [ "$NOGUI" != "yes" ];
+	then
+		sudo -u $NOTIFYUSER \
+			DISPLAY=:0 \
+			DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$USERID/bus \
+			notify-send --urgency=$3 "$1" "$2"
+	fi
 }
 
 weird_notify_user_formated() {
-        notify_user \
-                "Weird return for command '$1', check log with journalctl" \
-                "returned code: $2" \
-                $3
+	notify_user \
+		"Weird return for command '$1', check log with journalctl" \
+		"returned code: $2" \
+		$3
 }
 # ### # ### # ### #
 
 make_attempts() {
-        ATTEMPTS=$1
-        echo $6
-        while ! [ $ATTEMPTS -eq $2 ];
-        do
-                RESULT=$(eval $4)
-                if eval $5 ;
-                then
-                        sleep $7
-                        ATTEMPTS=$((ATTEMPTS+1))
-                        echo "attempt number: $ATTEMPTS"
-                else
-                        echo "success"
-                        break
-                fi
-        done
+	ATTEMPTS=$1
+	echo $6
+	while ! [ $ATTEMPTS -eq $2 ];
+	do
+		RESULT=$(eval $4)
+		if eval $5 ;
+		then
+			sleep $7
+			ATTEMPTS=$((ATTEMPTS+1))
+			echo "attempt number: $ATTEMPTS"
+		else
+			echo "success"
+			break
+		fi
+	done
 
-        if [ $ATTEMPTS -eq $2 ]; 
-        then
-                echo "max $3 attempts reached without anything returned"
-                notify_user "max $3 attempts reached" "nothing returned, checkout using journalctl" critical
-                exit
-        fi
+	if [ $ATTEMPTS -eq $2 ];
+	then
+		echo "max $3 attempts reached without anything returned"
+		notify_user "max $3 attempts reached" "nothing returned, checkout using journalctl" critical
+		exit
+	fi
 }
 
 write_to_txt_file() {
@@ -85,39 +85,39 @@ write_to_txt_file() {
 }
 
 do_firecfg() {
-        if [ "$FIRECFG" == "yes" ];
-        then
-                firecfg
-        fi
+	if [ "$FIRECFG" == "yes" ];
+	then
+		firecfg
+	fi
 }
 
 make_attempts \
-        1 \
-        20 \
-        "ping" \
-        "ping -c 1 google.com" \
-        '[ "$?" -ne 0 ]' \
-        "attempting ping to determine connectivity" \
-        5
+	1 \
+	20 \
+	"ping" \
+	"ping -c 1 google.com" \
+	'[ "$?" -ne 0 ]' \
+	"attempting ping to determine connectivity" \
+	5
 
 if [ "$NOGUI" != "yes" ];
 then
-        make_attempts \
-                1 \
-                20 \
-                "who" \
-                "who | grep $NOTIFYUSER" \
-                '[ -z "$RESULT" ]' \
-                "attempting who to determine is user logged in" \
-                60
+	make_attempts \
+		1 \
+		20 \
+		"who" \
+		"who | grep $NOTIFYUSER" \
+		'[ -z "$RESULT" ]' \
+		"attempting who to determine is user logged in" \
+		60
 fi
 
 PREPOSTFILE=opensuse_custaup_prepost.sh
 
 if [ -x "$PREPOSTFILE" ];
 then
-        source ./$PREPOSTFILE
-        pre_update
+	source ./$PREPOSTFILE
+	pre_update
 fi
 
 if [[ ! -v $REFRESHRETRY ]];
@@ -133,81 +133,81 @@ ZypRebNeed=102
 # ### # ### # ### #
 for i in $(seq 1 $REFRESHRETRY);
 do
-        # Updating starts here
-        zypper refresh
-        ZypRefRet=$?
-        if [ "$ZypRefRet" -eq "$ZypOK" ];
-        then    
-                if [ "$TUMBLEWEED" == "yes" ];
-                then
-                        UPCOMMAND=dup
-                else
-                        UPCOMMAND=update
-                fi
+	# Updating starts here
+	zypper refresh
+	ZypRefRet=$?
+	if [ "$ZypRefRet" -eq "$ZypOK" ];
+	then
+		if [ "$TUMBLEWEED" == "yes" ];
+		then
+			UPCOMMAND=dup
+		else
+			UPCOMMAND=update
+		fi
 
-                AutoAgree=''
-                if [ "$AGREELICENSE" == "yes" ];
-                then
-                       AutoAgree=--auto-agree-with-licenses 
-                fi
+		AutoAgree=''
+		if [ "$AGREELICENSE" == "yes" ];
+		then
+		       AutoAgree=--auto-agree-with-licenses
+		fi
 
-                ChangeArch=''
-                if [ "$CHANGEARCH" == "yes" ];
-                then
-                       ChangeArch=--allow-arch-change
-                fi
+		ChangeArch=''
+		if [ "$CHANGEARCH" == "yes" ];
+		then
+		       ChangeArch=--allow-arch-change
+		fi
 
-                ChangeVendor=''
-                if [ "$CHANGEVENDOR" == "yes" ];
-                then
-                       ChangeVendor=--allow-vendor-change
-                fi
+		ChangeVendor=''
+		if [ "$CHANGEVENDOR" == "yes" ];
+		then
+		       ChangeVendor=--allow-vendor-change
+		fi
 
-                echo "Running: zypper --non-interactive $UPCOMMAND $AutoAgree $ChangeArch $ChangeVendor"
-                zypper --non-interactive $UPCOMMAND $AutoAgree $ChangeArch $ChangeVendor 
+		echo "Running: zypper --non-interactive $UPCOMMAND $AutoAgree $ChangeArch $ChangeVendor"
+		zypper --non-interactive $UPCOMMAND $AutoAgree $ChangeArch $ChangeVendor
 
-                ZypDupRet=$?
-                if [ "$ZypDupRet" -eq "$ZypOK" ];
-                then
-                        do_firecfg
-                        write_to_txt_file
+		ZypDupRet=$?
+		if [ "$ZypDupRet" -eq "$ZypOK" ];
+		then
+			do_firecfg
+			write_to_txt_file
 
-                        if [ -x "$PREPOSTFILE" ];
-                        then
-                                post_update
-                        fi
+			if [ -x "$PREPOSTFILE" ];
+			then
+				post_update
+			fi
 
-                        zypper needs-rebooting
-                        ZypNeedRet=$?
-                        if [ "$ZypNeedRet" -eq "$ZypOK" ];
-                        then
-                                notify_user "Everything updated" "NO need for reboot" normal
-                                if [ "$ALWAYSRESTART" == "yes" ];
-                                then
-                                        reboot
-                                fi
+			zypper needs-rebooting
+			ZypNeedRet=$?
+			if [ "$ZypNeedRet" -eq "$ZypOK" ];
+			then
+				notify_user "Everything updated" "NO need for reboot" normal
+				if [ "$ALWAYSRESTART" == "yes" ];
+				then
+					reboot
+				fi
 
-                                break
-                        elif  [ "$ZypNeedRet" -eq "$ZypRebNeed" ];
-                        then
-                                notify_user "Everything updated" "NEEDS reboot" critical
-                                if [[ "$AUTORESTART" == "yes" || "$ALWAYSRESTART" == "yes" ]];
-                                then
-                                        reboot
-                                fi
+				break
+			elif  [ "$ZypNeedRet" -eq "$ZypRebNeed" ];
+			then
+				notify_user "Everything updated" "NEEDS reboot" critical
+				if [[ "$AUTORESTART" == "yes" || "$ALWAYSRESTART" == "yes" ]];
+				then
+					reboot
+				fi
 
-                                break
-                        else
-                                weird_notify_user_formated "zypper needs-reboot" "$ZypNeedRet" critical
-                                break
-                        fi
-                else
-                        weird_notify_user_formated "zypper $UPCOMMAND" "$ZypDupRet" critical
-                        break
-                fi
-        else
-                weird_notify_user_formated "zypper refresh" "$ZypRefRet" critical
-                sleep 60
-        fi
-        # ### # ### # ### #
+				break
+			else
+				weird_notify_user_formated "zypper needs-reboot" "$ZypNeedRet" critical
+				break
+			fi
+		else
+			weird_notify_user_formated "zypper $UPCOMMAND" "$ZypDupRet" critical
+			break
+		fi
+	else
+		weird_notify_user_formated "zypper refresh" "$ZypRefRet" critical
+		sleep 60
+	fi
+	# ### # ### # ### #
 done
